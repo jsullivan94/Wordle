4ca1b24f3a75b147bb58eb48efb38d8980c2233d
CHBDNW <130927495+CHBDNW@users.noreply.github.com>
2023-06-29 12:10:12 -0400

    new merge

--- word_list.py
+++ word_list.py
@@ -1,2 +1,12 @@
+import sqlite3
+CONN = sqlite3.connect('words.db')
+CURSOR = CONN.cursor()
 
+query = '''
+SELECT words
+FROM word;
+'''
+all_words = CURSOR.execute(query).fetchall()
+word_list = [word[0] for word in all_words]
+CONN.close()
 
--- wordle.py
+++ wordle.py
@@ -1,20 +1,21 @@
+from ascii import *
 import random 
+from word_list import word_list
 
-word_list = ['ABOUT', 'ABOVE', 'ABUSE', 'ACTOR', 'ACUTE', 'ADMIT', 'ADOPT', 'ADULT', 'AFTER', 'AGAIN', 'AGENT', 'AGREE', 'AHEAD', 'ALARM', 'ALBUM', 'ALERT', 'ALIKE', 'ALIVE', 'ALLOW', 'ALONE', 'ALONG', 'ALTER', 'AMONG', 'ANGER', 'ANGLE', 'ANGRY', 'APART', 'APPLE', 'APPLY', 'ARENA', 'ARGUE', 'ARISE', 'ARRAY', 'ASIDE', 'ASSET', 'AUDIO', 'AUDIT', 'AVOID', 'AWARD', 'AWARE', 'BADLY', 'BAKER', 'BASES', 'BASIC', 'BASIS', 'BEACH', 'BEGAN', 'BEGIN', 'BEGUN', 'BEING', 'BELOW', 'BENCH', 'BILLY', 'BIRTH', 'BLACK', 'BLAME', 'BLIND', 'BLOCK', 'BLOOD', 'BOARD', 'BOOST', 'BOOTH', 'BOUND', 'BRAIN', 'BRAND', 'BREAD', 'BREAK', 'BREED', 'BRIEF', 'BRING', 'BROAD', 'BROKE', 'BROWN', 'BUILD', 'BUILT', 'BUYER', 'CABLE', 'CALIF', 'CARRY', 'CATCH', 'CAUSE', 'CHAIN', 'CHAIR', 'CHART', 'CHASE', 'CHEAP', 'CHECK', 'CHEST', 'CHIEF', 'CHILD', 'CHINA', 'CHOSE', 'CIVIL', 'CLAIM', 'CLASS', 'CLEAN', 'CLEAR', 'CLICK', 'CLOCK', 'CLOSE', 'COACH', 'COAST', 'COULD', 'COUNT', 'COURT', 'COVER', 'CRAFT', 'CRASH', 'CREAM', 'CRIME', 'CROSS', 'CROWD', 'CROWN', 'CURVE', 'CYCLE', 'DAILY', 'DANCE', 'DATED', 'DEALT', 'DEATH', 'DEBUT', 'DELAY', 'DEPTH',]
+wordle_animation()
 
-random_word = random.choice(word_list) 
+random_word = random.choice(word_list).lower()
 print("Random word:", random_word)
 guesses = 6
 
 while guesses > 0:
-    user_word = input("Enter a word: ").upper()
+    user_word = input("Enter a word: ")
     if len(user_word) != len(random_word):
         print("Invalid length.")
         continue
-    if user_word not in word_list:
-        print("Invalid Word")
-        continue
-    
+    # if user_word not in word_list:
+    #     print("Invalid word.")
+    #     continue
     updated_word = ['_'] * len(user_word)  # Initialize the updated word with underscores because it's easier
 
     for i in range(len(user_word)):
@@ -26,17 +27,27 @@ while guesses > 0:
     print(' '.join(updated_word)) #.join combines elements to a string separated by spaces
 
     if user_word == random_word:
-        print("YOU WON!")
+        print(r"""
+                                                                                #             
+     #     #                             #     #                                #             
+      #   #                              #     #                                #             
+       # #     #####   #     #           #     #   #####   # ####               #             
+        #     #     #  #     #           #  #  #  #     #  ##    #              #             
+        #     #     #  #     #           # # # #  #     #  #     #              #             
+        #     #     #  #    ##           ##   ##  #     #  #     #                            
+        #      #####    #### #           #     #   #####   #     #              #             
+                                                                                          
+                                                                                          """)
         break
+
 
     guesses -= 1
     print("Remaining guesses:", guesses)
 
     if guesses == 0:
         random_word = "\033[95m" + random_word + "\033[0m" #prints in Purple
-        print("You lost. The word was", random_word)
+        print("Out of guesses. The word was", random_word)
         break
 
     # flex goals -- get it to not highlight yellow color if the letter has been used already
-    #  get it to only use valid words i.e. words in the word_list.py DONE
-    # print a word_pool of the alphabet but update it to colorize letters that you've already used
+    #  get it to only use valid words i.e. words in the word_list.py 
